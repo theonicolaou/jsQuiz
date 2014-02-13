df3509a90e76abf978d893213e199560099a7f5e
Theo Nicolaou <theo.nicolaou@capgemini.com>
2014-02-13 15:35:03 +0000

    (WIP) - add function for Next button to display questions one by one

--- js/quiz.js
+++ js/quiz.js
@@ -31,24 +31,58 @@ var EventUtil = {
 	}
 }
 
+//Initialise element ID's
+var wrapper = document.getElementById("wrapper");
+var quizIntro = document.getElementById("quizIntro");
+var quizQuestion = document.getElementById("quizQuestion");
+var quizChoices = document.getElementById("quizChoices");
+
 
 var quiz = {
 	allQuestions: [
 		{
-			question: "this is question 1",
+			question: "question 1",
 			choices: ["choice 1.1", "choice 1.2", "choice 1.3"],
 			correctAnswer: "correct answer 1"
 		},
 		{
-			question: "this is question 2",
+			question: "question 2",
 			choices: ["choice 2.1", "choice 2.2", "choice 2.3"],
 			correctAnswer: "correct answer 2"
+		},
+		{
+			question: "question 3",
+			choices: ["choice 3.1", "choice 3.2", "choice 3.3"],
+			correctAnswer: "correct answer 3"
 		}
 	],
+	// allQuestions: ["question 1", "question 2", "question 3"],
+
+	nextQuestion: function() {
+		for (var i = 0; i < quiz.allQuestions.length; i++) {
+			//console.log(quiz.allQuestions);
+			quizIntro.innerHTML = "";
+			quizQuestion.innerHTML = quiz.allQuestions[i].question;
+			quizChoices.innerHTML = "your choices are " + quiz.allQuestions[i].choices;
+			// + " "	+ " the correct answer is " + quiz.allQuestions[i].correctAnswer";
+		}
+	},
 }
 
-for (var i = 0; i < quiz.allQuestions.length; i++) {
-	console.log(quiz.allQuestions[i].question + ":" + " "
-		+ "your choices are " + quiz.allQuestions[i].choices + " "
-		+ "the correct answer is " + quiz.allQuestions[i].correctAnswer);
-}
+quizIntro.innerHTML = "Welcome to the quiz. There are " + quiz.allQuestions.length + " questions in the quiz";
+
+
+
+
+//keep this for later - capturing values of radio buttons
+// window.onload = function() {
+//   var theform = document.getElementById("myForm");
+//   var radioArray = [];
+//   console.log("-------------");
+//   theform.onchange = function() {
+//     var selectedRadio = document.querySelector('input[name="color"]:checked').value;
+//     console.log("you selected", selectedRadio);
+//     radioArray.push(selectedRadio);
+//     console.log("radioArray is now " + radioArray);
+//   };
+// };
